1830cf321f691821186dadc38221abbce9d2e6b3
Angelo Burog <angelokburog30@gmail.com>
2024-07-26 00:07:35 +0800

    feat: get method for all shows & simple pagination

--- index.js
+++ index.js
@@ -26,6 +26,22 @@ app.get("/home", async (req, res) => {
   res.render("home.ejs");
 });
 
+app.get("/show", async (req, res) => {
+  try {
+    const response = await axios.get(TV_URL + "/shows");
+    const page = req.query.page;
+    const limit = req.query.limit;
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const asd = response.data;
+
+    const middle = asd.slice(startIndex, endIndex);
+    console.log(middle);
+    res.render("home.ejs", { content: middle });
+  } catch (error) {}
+});
+
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
